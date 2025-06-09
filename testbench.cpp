3c5d3d8493ddb5ff04a8a9303034274eb49d170e
HoThienDuy <105864108+HoThienDuy@users.noreply.github.com>
2025-06-09 15:51:21 +0700

    Update testbench.cpp

--- testbench.cpp
+++ testbench.cpp
@@ -10,13 +10,12 @@
 
 using namespace std;
 
-typedef ap_fixed<16,2,AP_RND,AP_SAT> DTYPE_OUT;
-typedef ap_fixed<16,2,AP_RND,AP_SAT> DTYPE_IN;
+
 
 int main()
 {
     DTYPE_IN input[16384];     // 128 x 128 (ảnh đầu vào)
-    DTYPE_OUT output[6 *6 * 64]; // Kết quả từ lớp Conv2D thứ hai
+    DTYPE_OUT output[1]; // Kết quả từ lớp Conv2D thứ hai
 
 
 
@@ -31,18 +30,26 @@ int main()
     DTYPE_IN bias3[32];
     DTYPE_IN kernel4[18432];
     DTYPE_IN bias4[64];
+    DTYPE_IN kernel5[230400];
+    DTYPE_IN bias5[100];
+    DTYPE_IN kernel6[100];
+    DTYPE_IN bias6[1];
 
     // Mở file chứa input, kernel, bias
-    FILE *fp = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\input_image.txt", "r");
-    FILE *fp_kernel1 = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\Khoaluan\\conv2d_weights.txt", "r");
-    FILE *fp_bias1 = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\Khoaluan\\conv2d_bias.txt", "r");
-    FILE *fp_kernel2 = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\Khoaluan\\conv2d_1_weights.txt", "r");
-    FILE *fp_bias2 = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\Khoaluan\\conv2d_1_bias.txt", "r");
-    FILE *fp_kernel3 = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\Khoaluan\\conv2d_2_weights.txt", "r");
-    FILE *fp_bias3 = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\Khoaluan\\conv2d_2_bias.txt", "r");
-    FILE *fp_kernel4 = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\Khoaluan\\conv2d_3_weights.txt", "r");
-    FILE *fp_bias4 = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\Khoaluan\\conv2d_3_bias.txt", "r");
-    FILE *fp_output = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\dst_conv2.txt", "w");
+    FILE *fp = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\Healthy.txt", "r");
+    FILE *fp_kernel1 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\conv2d_4_weights.txt", "r");
+    FILE *fp_bias1 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\conv2d_4_bias.txt", "r");
+    FILE *fp_kernel2 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\conv2d_5_weights.txt", "r");
+    FILE *fp_bias2 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\conv2d_5_bias.txt", "r");
+    FILE *fp_kernel3 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\conv2d_6_weights.txt", "r");
+    FILE *fp_bias3 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\conv2d_6_bias.txt", "r");
+    FILE *fp_kernel4 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\conv2d_7_weights.txt", "r");
+    FILE *fp_bias4 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\conv2d_7_bias.txt", "r");
+    FILE *fp_kernel5 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\dense_2_weights.txt", "r");
+    FILE *fp_bias5 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\dense_2_bias.txt", "r");
+    FILE *fp_kernel6 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\dense_3_weights.txt", "r");
+    FILE *fp_bias6 = fopen("C:\\Users\\ADMIN88\\Downloads\\weights_output_cot\\dense_3_bias.txt", "r");
+    FILE *fp_output = fopen("C:\\Users\\ADMIN88\\Downloads\\In_out\\dst2.txt", "w");
 
 
     float temp;
@@ -98,6 +105,29 @@ int main()
             fscanf(fp_bias4, "%f", &temp);
             bias4[i] = (DTYPE_IN) temp;
         }
+//         Đọc kernel2 từ file (1152 giá trị)
+           for (int i = 0; i < 230400; i++) {
+               fscanf(fp_kernel5, "%f", &temp);
+               kernel5[i] = (DTYPE_IN) temp;
+           }
+
+           // Đọc bias2 từ file (16 giá trị)
+           for (int i = 0; i < 100; i++) {
+               fscanf(fp_bias5, "%f", &temp);
+               bias5[i] = (DTYPE_IN) temp;
+           }
+           for (int i = 0; i < 100; i++) {
+                  fscanf(fp_kernel6, "%f", &temp);
+                  kernel6[i] = (DTYPE_IN) temp;
+              }
+
+              // Đọc bias2 từ file (16 giá trị)
+              for (int i = 0; i < 1; i++) {
+                  fscanf(fp_bias6, "%f", &temp);
+                  bias6[i] = (DTYPE_IN) temp;
+              }
+
+       // Đóng fi
 
     // Đóng file đầu vào sau khi đọc xong
     fclose(fp);
@@ -109,20 +139,24 @@ int main()
     fclose(fp_bias3);
     fclose(fp_kernel4);
     fclose(fp_bias4);
+    fclose(fp_kernel5);
+    fclose(fp_bias5);
+    fclose(fp_kernel6);
+    fclose(fp_bias6);
     // Gọi hàm xử lý Conv2D thứ hai
-    filter2d_accel(kernel1,kernel2,kernel3,kernel4,bias1, bias2,bias3,bias4, input, output);
+    CNN(kernel1,kernel2,kernel3,kernel4,kernel5,kernel6,bias1,bias2,bias3,bias4,bias5,bias6,input, output);
 
     // Ghi kết quả từ Conv2D thứ hai vào file
-    for (int k = 0; k < 64; k++) {
-        fprintf(fp_output, "Kênh [%d]:\n", k);
+//    for (int k = 0; k < 64; k++) {
+//        fprintf(fp_output, "Kênh [%d]:\n", k);
 
-        for (int i = 0; i < 36; i++) { // 61x61 = 3721
-            fprintf(fp_output, "%.14f\n", (float)output[k * 36 + i]);
+        for (int i = 0; i < 1; i++) { // 61x61 = 3721
+            fprintf(fp_output, "%.14f\n", (float)output[i]);
         }
 
-        fprintf(fp_output, "\n"); // Xuống dòng sau mỗi kênh
-    }
-    fclose(fp_output);
+        fprintf(fp_output, "\n"); // Xuống dòng sau mỗi kên
+//    }
+//    fclose(fp_output);
 
     printf("Processing completed. Outputs saved to dst_conv2.txt.\n");
 
